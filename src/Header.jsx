62e645b156378e3543d25dd2adcc4a79836d7d57
matias.zurita06 <matias.zurita06@inacapmail.cl>
2025-07-27 00:26:13 -0400

    Modificacion de Header

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 
 export function Header() {
   return (
-    <header>
-      <h1>Gestion de Tareas</h1>
+    <header className="navbar navbar-dark bg-primary mb-4 p-3 shadow-sm">
+      <div className="container-fluid d-flex justify-content-center align-items-center">
+        <a className="navbar-brand mb-0 h1 fs-3" href="#">😎 MI LISTA DE TAREAS 😎</a>
+      </div>
     </header>
   );
 }
